b8f458ad3b0952dd10e2eb1162fa9541b30bd270
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-13 12:01:06 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+
+MODEL_NAME = "meta-llama/Llama-2-7b-chat-hf"
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+FAISS_INDEX_PATH = "data/faiss_index"
+DATA_PATH = "data/sample_fault_logs.csv"
+NER_MODEL = "en_core_web_sm"
